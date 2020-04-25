442b69a99d691c596768f905c717cbff9d518f5c
Michael Finch <michael.donovan.finch@gmail.com>
2020-04-25 14:04:23 -0700

    add post validations, generate post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    before_validation(:capitalize_title)
+
+    validates(:title, presence: true, uniqness: true )
+    validates(:body, 
+        presence: {message: "Body must be longer than 50 characters", length: {minimum: 50}}
+    )
+
+private
+
+    def capitalize_title
+        self.title = self.title.capitalize
+    end
+
 end
+
+git add . && git commit -m ""
