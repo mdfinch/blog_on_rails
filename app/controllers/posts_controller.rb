c4dc125534f56f40621af6c2f90a2a15f541f702
Michael Finch <michael.donovan.finch@gmail.com>
2020-04-25 14:31:17 -0700

    add posts routes, index action, index.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,29 @@
 class PostsController < ApplicationController
+    def index
+        @posts = Post.all.order("id ASC")
+    end
+
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+    def show
+        
+    end
+
+    def destroy
+        
+    end
+
+    def edit
+        
+    end
     
+    def update
+        
+    end
 end
