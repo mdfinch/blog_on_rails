bb4ee264b0b14b254298f96db439bc7926691116
Michael Finch <michael.donovan.finch@gmail.com>
2020-04-25 15:38:23 -0700

    add new page, form partial, create action, new action, fix index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,20 @@
 class PostsController < ApplicationController
     def index
-        @posts = Post.all.order("id ASC")
+        @posts = Post.all.order("id DESC")
     end
 
     def new
-        
+        @post = Post.new 
+
     end
 
     def create
-        
+        @post = Post.new(params.require(:post).permit(:title, :body))
+        if @post.save
+            redirect_to posts_path
+        else
+            render :new
+        end
     end
 
     def show
