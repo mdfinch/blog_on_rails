bde41303da9b632c029a580cfdace7f8e994e20c
Michael Finch <michael.donovan.finch@gmail.com>
2020-04-30 23:14:47 -0700

    add comment seed, fix navbar css, add if statement for _form page, fix css on show page and add time ago, add width to cards on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Post.delete_all
+Comment.delete_all
 
 NUM_POST = 50
 
@@ -10,8 +11,18 @@ NUM_POST.times do
         created_at: created_at,
         updated_at: created_at
     )
+    p.comments = rand(1..3).times.map do
+        Comment.new(
+        body: Faker::GreekPhilosophers.quote,
+        created_at: created_at,
+        updated_at: created_at
+    )
+    end
 end
 
 post = Post.all 
+comment = Comment.all
 
 puts Cowsay.say("Generated #{post.count} posts", :frogs)
+puts Cowsay.say("Generated #{comment.count} comments", :tux)
+
