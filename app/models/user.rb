dd3eb46d2da9ae30ea96a4b6ae11aeec142867af
Michael Finch <michael.donovan.finch@gmail.com>
2020-05-03 11:52:29 -0700

    add bcrypt gem, add relations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
 
     validates :email, presence: true, uniqueness: true,
     format:  /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     
+    has_many :posts
+    has_many :comments
 end
